df8a490b5417235b53f2b9df8f33d841e39936fc
Stefano <ana28@aol.com>
2017-03-22 01:44:43 -0300

    Added data to the posts collection & Wired collection into `postsList` template.

--- client/templates/posts/postsList.js
+++ client/templates/posts/postsList.js
@@ -1,18 +1,5 @@
-var postsData = [
-  {
-    title: "Introduction to Telescope",
-    url:'http://introductiontotelescope.com'
-  },
-  {
-    title: "Meteor",
-    url:'http://meteor.com'
-  },
-  {
-    title: "The book of Meteor",
-    url:'http://thebookofmeteor.com'
-  }
-]
-
 Template.postsList.helpers({
-  posts: postsData
-})
+  posts: function() {
+    return Posts.find();
+  }
+});
